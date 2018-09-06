32e4051979c5cd89a91e5f14544171fb21b619d8
Taras Labiak <kissarat@gmail.com>
2018-09-07 02:24:57 +0300

    + vue.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function main() {
-  const isProduction = 'kissarat.github.io' === location.hostname
+  const isProduction = /(kissarat.git(hub|lab).io|administer.tech)$/.test(location.hostname)
   if (isProduction) {
     document.getElementById('avatar').src = 'https://grabify.link/G4PVBQ'
   }
+
+  new Vue({
+    el: '#app',
+    data: {
+      large: false
+    }
+  })
 }
 
 document.addEventListener('DOMContentLoaded', main)
