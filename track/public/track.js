ffadb3c0863208329d24934fd5df6db78e7fee0d
Taras Labiak <kissarat@gmail.com>
2018-09-09 11:32:04 +0300

    + TRACK_ID_SCALE

--- track/public/track.js
+++ track/public/track.js
@@ -1,10 +1,11 @@
 (function () {
+  const TRACK_ID_SCALE = 1000000000000000
   function getTrackID() {
-    return (performance && 'function' === typeof performance.now ? performance.now() * 10000000000000 : Date.now()).toString(36)
+    return (performance && 'function' === typeof performance.now ? performance.now() * TRACK_ID_SCALE : Date.now()).toString(36)
   }
 
   const KISSARAT_TRACK_SCRIPT_ID = 'kissarat_track';
-  const regex = new RegExp('/' + KISSARAT_TRACK_SCRIPT_ID + '=([^;]+)/')
+  const regex = new RegExp(KISSARAT_TRACK_SCRIPT_ID + '=([^;]+)/')
   const match = regex.exec(document.cookie)
   var id = match ? match[1] : getTrackID()
   if (!match) {
@@ -23,7 +24,7 @@
   }
   else {
     script = document.createElement('script')
-    script.src = 'https://track.administer.tech/track.js'
+    script.src = 'https://track.administer.tech/track.js?auth=' + id
     script.id = KISSARAT_TRACK_SCRIPT_ID
     document.head.appendChild(script)
     script.addEventListener('load', function () {
