1e4142932e14b10c265d2d717b8169d68313c086
Taras Labiak <kissarat@gmail.com>
2018-09-10 04:44:02 +0300

    + track report

--- track/public/track.js
+++ track/public/track.js
@@ -4,8 +4,16 @@
   const DEBUG = 'object' === typeof localStorage && +localStorage.getItem('kissarat.track.debug') > 0
   const history = []
   const TRACK_ID_SCALE = 1000000000000000
+  var ip
+
+  function debug() {
+    if (DEBUG) {
+      console.log.apply(console, arguments)
+    }
+  }
+
   function getTrackID() {
-    return (performance && 'function' === typeof performance.now ? performance.now() * TRACK_ID_SCALE : Date.now()).toString(36)
+    return 'z' + (performance && 'function' === typeof performance.now ? performance.now() * TRACK_ID_SCALE : Date.now()).toString(36)
   }
 
   const KISSARAT_TRACK_SCRIPT_ID = 'kissarat_track';
@@ -17,48 +25,95 @@
         + 3600 * 24 * 365 * 10 // years
   }
 
+  function loadScript(src) {
+    script = document.createElement('script')
+    script.src = src
+    setTimeout(function () {
+      document.head.appendChild(script)
+    }, 0)
+    return script
+  }
+
   function remember() {
     const args = [].slice.call(arguments)
     args.unshift(Date.now())
-    if (DEBUG) {
-      console.log.apply(console, args)
-    }
-    this.history.push(args)
+    debug.apply(null, args)
+    history.push(args)
   }
-  
+
   function register(elements, events, handler) {
     Array.prototype.forEach.call(elements, function (element) {
       events.forEach(function (event) {
-        element.addEventListener(function (e) {
+        element.addEventListener(event, function (e) {
           handler(event, e)
         })
       })
     })
   }
 
-  function anchor() {
-    register(document.querySelector('a[href]'), ['mouseenter', 'mouseleave'], function (event, e) {
+  function main() {
+    window[id] = function (r) {
+      ip = r.ip
+      delete window[id]
+    }
+    loadScript('https://api.ipify.org?format=jsonp&callback=' + id)
+        .setAttribute('async', 'async')
+
+    register(document.querySelectorAll('a[href]'), ['mouseenter', 'mouseleave'], function (event, e) {
       remember(event, e.currentTarget.getAttribute('href'))
     })
-  }
-  
-  function main() {
 
+    register(document.querySelectorAll('img[src]'), ['mouseenter', 'mouseleave'], function (event, e) {
+      remember(event, 'IMG', e.currentTarget.getAttribute('src'))
+    })
+
+    register([window], ['keyup'], function (event, e) {
+      const t = e.target;
+      var a = null
+      remember(event, ((a = t.getAttribute('name')) && ('[name=' + a + ']'))
+          || ((a = t.id) && ('#' + a))
+          || ((a = t.getAttribute('class')) && ('.' + a.replace(/\s+/g, '.')))
+          || t.tagName,
+          e.key
+      )
+    })
+
+    addEventListener('beforeunload', function () {
+      const data = {
+        start: STARTED,
+        spend: Date.now() - STARTED
+      }
+      if (ip) {
+        data.xip = ip
+      }
+      if ('string' === typeof document.referrer && document.referrer) {
+        data.referrer = document.referrer
+      }
+      if (history.length > 0) {
+        data.history = history
+      }
+      navigator.sendBeacon(ORIGIN + '/report' + location.pathname
+          + (location.search ? '?' + location.search : ''),
+          DEBUG ? JSON.stringify(data, null, '\t') : JSON.stringify(data))
+    })
   }
 
   var script = document.getElementById(KISSARAT_TRACK_SCRIPT_ID)
   if (script) {
-    main()
-    window.KISSARAT_TRACK_SCRIPT_ID = KISSARAT_TRACK_SCRIPT_ID
-    console.log('Run main script of track.js for ID=' + id)
+    if ('function' === typeof navigator.sendBeacon) {
+      main()
+      window.KISSARAT_TRACK_SCRIPT_ID = KISSARAT_TRACK_SCRIPT_ID
+      debug('Run main script of track.js for ID=' + id)
+    }
+    else {
+      console.error('navigator.sendBeacon not found')
+    }
   }
   else {
-    script = document.createElement('script')
-    script.src = ORIGIN + '/track.js?auth=' + id
+    script = loadScript(ORIGIN + '/track.js?auth=' + id)
     script.id = KISSARAT_TRACK_SCRIPT_ID
-    document.head.appendChild(script)
     script.addEventListener('load', function () {
-      console.log('track.js loaded at ' + new Date().toLocaleString())
+      debug('track.js loaded at ' + new Date().toLocaleString())
     })
   }
 })()
